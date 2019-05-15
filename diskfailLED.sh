ab5e34d2c66b42c1cd99bff3a20d941733c3d9a1
Ender117 <ender117@qq.com>
2019-05-15 14:40:48 -0400

    Update diskfailLED.sh

--- diskfailLED.sh
+++ diskfailLED.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
+
+
+
 if [ ! "$1" ]; then
   echo "Usage: diskfailLED.sh [pool] "
   echo "Scan a pool, activate leds of failed drives"
   exit
 fi
+
 pool="$1"
 
+tmpdir=/tmp/diskfailLED."$pool"
+mkdir "$tmpdir"
+
+trap 'rm -rf "$tmpdir"' INT TERM EXIT
+
+
+
+glabel status | awk '{print "s|"$1"|"$3"\t\t\t	  |g"}' > "$tmpdir"/glabel-lookup.sed # prepare translate gptid to geoms
 condition=$(/sbin/zpool status $pool | egrep -i '(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED|corrupt|cannot|unrecover)')
+
 
 if [ "${condition}" ]; then
 
 
-echo >&2 "$pool" "unhealthy, scanning for failed drive(s)."
+echo >&2 "Pool" "$pool" "unhealthy, scanning for failed drive(s)."
 
-glabel status | awk '{print "s|"$1"|"$3"\t\t\t	  |g"}' > /tmp/glabel-lookup.sed # prepare translate gptid to geoms
 
-zpool status "$pool" | awk -F'was /dev/' '{print $2}' |  sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> /tmp/faileddisk.sed # look for drives that appended with was /dev/... (uaually offline/removed/unavail state) and write them into /tmp/faileddisk.sed
-zpool status "$pool" | grep -E "(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> /tmp/faileddisk.sed # look for other failed drives and write them into /tmp/faileddisk.sed
+
+zpool status "$pool" | awk -F'was /dev/' '{print $2}' |  sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for drives that appended with was /dev/... (uaually offline/removed/unavail state) and write them into "$tmpdir"/faileddisk.sed
+zpool status "$pool" | grep -E "(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for other failed drives and write them into "$tmpdir"/faileddisk.sed
 
 
 for faileddisk in $(cat /tmp/faileddisk.sed);
@@ -29,7 +42,7 @@ done
 
 else
 
-echo "$pool" "is healthy."
+echo "Pool" "$pool" "is healthy."
 #sesutil locate all off > /dev/null 2>&1
 
 
@@ -37,15 +50,15 @@ fi
 
 
 echo  "Turning off LEDs of good disks."
-zpool status "$pool" | grep -E "(ONLINE)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(ONLINE)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> /tmp/gooddisk.sed # look for good drives and write them into /tmp/gooddisk.sed
+zpool status "$pool" | grep -E "(ONLINE)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(ONLINE)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/gooddisk.sed # look for good drives and write them into "$tmpdir"/gooddisk.sed
 for gooddisk in $(cat /tmp/gooddisk.sed);
 do
 
-	sesutil locate $gooddisk off
+	sesutil locate $gooddisk off 2>/dev/null
+
+
 
 done
-
-trap 'rm -rf /tmp/glabel-lookup.sed;rm -rf /tmp/faileddisk.sed;rm -rf /tmp/gooddisk.sed;' INT TERM EXIT  
 
 
 
