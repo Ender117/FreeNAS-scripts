7c80c1790161ed511a602d277e3149dae946d10c
Ender117 <ender117@qq.com>
2019-05-15 14:43:38 -0400

    Update diskfailLED.sh

--- diskfailLED.sh
+++ diskfailLED.sh
@@ -28,11 +28,11 @@ echo >&2 "Pool" "$pool" "unhealthy, scanning for failed drive(s)."
 
 
 
-zpool status "$pool" | awk -F'was /dev/' '{print $2}' |  sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for drives that appended with was /dev/... (uaually offline/removed/unavail state) and write them into "$tmpdir"/faileddisk.sed
-zpool status "$pool" | grep -E "(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for other failed drives and write them into "$tmpdir"/faileddisk.sed
+zpool status "$pool" | awk -F'was /dev/' '{print $2}' |  sed -f "$tmpdir"/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for drives that appended with was /dev/... (uaually offline/removed/unavail state) and write them into "$tmpdir"/faileddisk.sed
+zpool status "$pool" | grep -E "(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(DEGRADED|FAULTED|OFFLINE|UNAVAIL|REMOVED|FAIL|DESTROYED)' '{print $1}' | sed -f "$tmpdir"/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/faileddisk.sed # look for other failed drives and write them into "$tmpdir"/faileddisk.sed
 
 
-for faileddisk in $(cat /tmp/faileddisk.sed);
+for faileddisk in $(cat "$tmpdir"/faileddisk.sed);
 do
 	echo >&2 $faileddisk "failed, trying to blink LED."
 	sesutil locate $faileddisk on
@@ -50,8 +50,8 @@ fi
 
 
 echo  "Turning off LEDs of good disks."
-zpool status "$pool" | grep -E "(ONLINE)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(ONLINE)' '{print $1}' | sed -f /tmp/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/gooddisk.sed # look for good drives and write them into "$tmpdir"/gooddisk.sed
-for gooddisk in $(cat /tmp/gooddisk.sed);
+zpool status "$pool" | grep -E "(ONLINE)" | grep -vE "($pool|NAME|mirror|raidz|stripe|logs|spares|state|replacing|was /dev/)" |awk -F'(ONLINE)' '{print $1}' | sed -f "$tmpdir"/glabel-lookup.sed | awk -F'p[0-9]' '{print $1}' | awk 'NF' >> "$tmpdir"/gooddisk.sed # look for good drives and write them into "$tmpdir"/gooddisk.sed
+for gooddisk in $(cat "$tmpdir"/gooddisk.sed);
 do
 
 	sesutil locate $gooddisk off 2>/dev/null
