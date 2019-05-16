2f1393334e182b593abcec9163bab03a0f26518d
Ender117 <ender117@qq.com>
2019-05-16 00:13:19 -0400

    Update FanControl.sh

--- FanControl.sh
+++ FanControl.sh
@@ -6,33 +6,39 @@ echo "$DATE"
 #
 
 STATICSPEEDBASE16="0x0f"
-STATICSPEEDBASE16="0x0f"
-TEMPTHRESHOLD="29"
-#
+
+CPUavgTupper="45"
+NICTupper="65"
+CPUavgTlower="40"
+NICTlower="60"
+
+CPUavgT=0
 ncpu=$( sysctl hw.ncpu | awk '{ print $2 }' )
-
-
-avg=0
 
 for c in `jot ${ncpu} 0`; do
  temp=$( sysctl dev.cpu.${c}.temperature | sed -e 's|.*: \([0-9.]*\)C|\1|' )
- avg=$( echo "${avg} + ${temp}" | bc )
+ CPUavgT=$( echo "${CPUavgT} + ${temp}" | bc )
 done
 	
-avg=$( echo "${avg} / (${ncpu})" | bc )
-rc=${avg}
+CPUavgT=$( echo "${CPUavgT} / (${ncpu})" | bc )
+echo "CPU temperature (average across all cores) is" ${CPUavgT} "C"
 
 
-echo "$IDRACIP: -- current temperature --"
-echo "$T"
+NICT=$(sysctl dev.t5nex.0.temperature|cut -f2 -d" ")
+echo "NIC temperature is" ${NICT} "C"
+
+
 #
-if [[ $T > $TEMPTHRESHOLD ]]
+if [[ $CPUavgT > $NICTupper || $NICT > $NICTupper ]]
   then
     echo "--> enable dynamic fan control"
     ipmitool raw 0x30 0x30 0x01 0x01
   else
+  if [[ $CPUavgT < $NICTlower || $NICT < $NICTlower ]]
+  then
     echo "--> disable dynamic fan control"
     ipmitool raw 0x30 0x30 0x01 0x00
     echo "--> set static fan speed"
     ipmitool raw 0x30 0x30 0x02 0xff $STATICSPEEDBASE16
+    fi
 fi
